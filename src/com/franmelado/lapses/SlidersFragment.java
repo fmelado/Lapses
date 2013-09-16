0624ed2c6e2e1570ef1e1ab4f2ec9776c726c091
Francisco Melado <francisco.melado.morillo@gmail.com>
2013-09-16 20:10:37 +0200

    Create SlidersFragment.java

---
+++ src/com/franmelado/lapses/SlidersFragment.java
@@ -0,0 +1,27 @@
+package com.franmelado.lapses;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SlidersFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_lapses,container,false);
+		setHasOptionsMenu(true);
+		return view;
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.activity_lapses, menu);
+	}
+
+}
