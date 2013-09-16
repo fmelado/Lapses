17caa142af9a748cb32b18aa5c575d7744bb1bba
Francisco Melado <francisco.melado.morillo@gmail.com>
2013-09-16 19:46:16 +0200

    Create HelpFragment.java
    Inflates activity_help.xml layout

---
+++ src/com/franmelado/lapses/HelpFragment.java
@@ -0,0 +1,22 @@
+package com.franmelado.lapses;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelpFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_help,container,false);
+		setHasOptionsMenu(true);
+		return view;
+	}
+
+}
