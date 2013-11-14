1fc1299cafce06a207210b232dd2bcbe15fa8955
fmelado <francisco.melado.morillo@gmail.com>
2013-11-14 18:58:46 +0100

    Clean fragments code, all errors

--- src/com/franmelado/lapses/HelpFragment.java
+++ src/com/franmelado/lapses/HelpFragment.java
@@ -5,6 +5,7 @@ import android.content.*;
 import android.net.*;
 import android.os.*;
 import android.view.*;
+import android.view.View.*;
 import android.widget.*;
 
 public class HelpFragment extends Fragment {
@@ -14,25 +15,25 @@ public class HelpFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_help,container,false);
 		setHasOptionsMenu(false);
+		ImageButton gplusbutton = (ImageButton) view.findViewById(R.id.gplusButton);
+		ImageButton gplv3button = (ImageButton) view.findViewById(R.id.gplv3Button);
+	
+	    gplusbutton.setOnClickListener(new OnClickListener() {
+	        @Override
+	        public void onClick (View v) {
+		        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about5)));
+		        startActivity(browserIntent);
+	        }
+	    });
+	
+	    gplv3button.setOnClickListener(new OnClickListener() {
+	        @Override
+	        public void onClick (View v) {
+		    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about7)));
+		    startActivity(browserIntent);
+	        }
+	    });
+		
 		return view;
 	}
-	
-	ImageButton gplusbutton = (ImageButton) findViewById(R.id.gplusButton);
-	gplusbutton.setOnClickListener(new OnClickListener() {
-	@Override
-	public void onClick (View v) {
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about5)));
-		startActivity(browserIntent);
-	}
-	});
-
-	ImageButton gplv3button = (ImageButton) findViewById(R.id.gplv3Button);
-	gplv3button.setOnClickListener(new OnClickListener()) {
-	@Override
-	public void onClick (View v) {
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about7)));
-		startActivity(browserIntent);
-	}
-	});
-
 }
--- src/com/franmelado/lapses/SlidersFragment.java
+++ src/com/franmelado/lapses/SlidersFragment.java
@@ -22,8 +22,8 @@ public class SlidersFragment extends Fragment {
 	TextView picTotal;
 	
 	@Override
-	public void onActivityCreated(){
-		super.onActivityCreated();
+	public void onActivityCreated(Bundle savedInstanceState){
+		super.onActivityCreated(savedInstanceState);
 		((LapsesActivity)this.getActivity()).getLapse();
 	}
 	
@@ -32,7 +32,6 @@ public class SlidersFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_sliders,container,false);
 		setHasOptionsMenu(false);
-		return view;
 		
 		/**
          * Initialization of variables
@@ -50,33 +49,34 @@ public class SlidersFragment extends Fragment {
 		*/
 		
         //Initialization of Shooting session - Duration
-        valueSSD = (TextView) findViewById(R.id.tvSSDurationV);
-        seekbarSSD = (SeekBar) findViewById(R.id.sbSSDuration);
-        seekbarSSD.setProgress(myLapse.getSsd() - 1);
-        valueSSD.setText(myLapse.formatS(myLapse.getSsd()));
+        valueSSD = (TextView) view.findViewById(R.id.tvSSDurationV);
+        seekbarSSD = (SeekBar) view.findViewById(R.id.sbSSDuration);
+        seekbarSSD.setProgress(((LapsesActivity)this.getActivity()).getLapse().getSsd() - 1);
+        valueSSD.setText(((LapsesActivity)this.getActivity()).getLapse().formatS(((LapsesActivity)this.getActivity()).getLapse().getSsd()));
         
         // Initialization of Shooting session - Interval
-        valueSSI = (TextView) findViewById(R.id.tvSSIntervalV);
-        seekbarSSI = (SeekBar) findViewById(R.id.sbSSInterval);
-        seekbarSSI.setProgress(myLapse.getSsi() - 1);
-        valueSSI.setText(myLapse.formatS(myLapse.getSsi()));
+        valueSSI = (TextView) view.findViewById(R.id.tvSSIntervalV);
+        seekbarSSI = (SeekBar) view.findViewById(R.id.sbSSInterval);
+        seekbarSSI.setProgress(((LapsesActivity)this.getActivity()).getLapse().getSsi() - 1);
+        valueSSI.setText(((LapsesActivity)this.getActivity()).getLapse().formatS(((LapsesActivity)this.getActivity()).getLapse().getSsi()));
         
         //Initialization of Final movie - Duration
-        valueFMD = (TextView) findViewById(R.id.tvFMDurationV);
-        seekbarFMD = (SeekBar) findViewById(R.id.sbFMDuration);
-        seekbarFMD.setProgress(myLapse.getFmd() - 1);
-        valueFMD.setText(myLapse.formatS(myLapse.getFmd()));
+        valueFMD = (TextView) view.findViewById(R.id.tvFMDurationV);
+        seekbarFMD = (SeekBar) view.findViewById(R.id.sbFMDuration);
+        seekbarFMD.setProgress(((LapsesActivity)this.getActivity()).getLapse().getFmd() - 1);
+        valueFMD.setText(((LapsesActivity)this.getActivity()).getLapse().formatS(((LapsesActivity)this.getActivity()).getLapse().getFmd()));
         
         //Initialization of Final movie - FPS
-        valueFMF = (TextView) findViewById(R.id.tvFMfpsV);
-        seekbarFMF = (SeekBar) findViewById(R.id.sbFMfps);
-        seekbarFMF.setProgress(myLapse.getFmf() - 1);
-        valueFMF.setText(myLapse.getFmf() + " fps");
+        valueFMF = (TextView) view.findViewById(R.id.tvFMfpsV);
+        seekbarFMF = (SeekBar) view.findViewById(R.id.sbFMfps);
+        seekbarFMF.setProgress(((LapsesActivity)this.getActivity()).getLapse().getFmf() - 1);
+        valueFMF.setText(((LapsesActivity)this.getActivity()).getLapse().getFmf() + " fps");
         
         //Initialization of pictotal
-        picTotal = (TextView) findViewById(R.id.tvNumpic);
-        picTotal.setText (myLapse.getPt() + " " + getString(R.string.pictotal));
+        picTotal = (TextView) view.findViewById(R.id.tvNumpic);
+        picTotal.setText (((LapsesActivity)this.getActivity()).getLapse().getPt() + " " + getString(R.string.pictotal));
 		
+	    return view;
 	}
 	
 	@Override
